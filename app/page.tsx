dfc813c1a483b781e3a2c68956705013cda06929
ubuntu-jatin <kineticenergy96@outlook.com>
2024-03-05 17:43:21 +0530

    some few changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-			<main className="h-lvh w-lvw flex flex-col flex-wrap">
-<div className="w-4/5 h-4/5 bg-gray-200">Main Function</div>
-<div className="w-4/5 h-1/5 bg-orange-200">Function Function</div>
-<div className="w-1/5 h-1/5 bg-blue-200">Space-Time function</div>
-<div className="w-1/5 h-4/5 bg-red-200">Coding function</div>
-			</main>
-      );
+    <main className="h-lvh w-lvw flex flex-col flex-wrap">
+      <div className="w-4/5 h-4/5 bg-gray-200">Main Function</div>
+      <div className="w-4/5 h-1/5 bg-orange-200">Function Function</div>
+      <div className="w-1/5 h-1/5 bg-blue-200">Space-Time function</div>
+      <div className="w-1/5 h-4/5 bg-red-200">Coding function</div>
+    </main>
+  );
 }
